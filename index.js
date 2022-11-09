51e77c3e9c810937e77a50de22adbda71de957c2
Daniel Rice <danielrice5627@gmail.com>
2022-11-09 13:02:14 -0800

    commit tree e64910352a452d7f6b6d7cb735968d83ec4b12e1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(someArray, event) {
+    const cardsArray = [];
+    for (let i = 0; i < someArray.length; i++){
+        cardsArray.push(`Thank you, ${someArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return cardsArray;
+}
+function countDown(x) {
+    let count=x;
+    while (count>=0){
+        console.log(count);
+        count--;
+    }
+
+}
